5d9c69edde43e53eae25c11fd7ea4ce52ad0769b
Doğukan Çalışkan <80880586+Dowukan@users.noreply.github.com>
2024-03-11 18:12:58 +0300

    Update Bolumler.sql

--- Bolumler.sql
+++ Bolumler.sql
@@ -1,12 +1,12 @@
 
 INSERT INTO Bolum (BolumID, BolumAdi, FakulteID)
 VALUES
-(1, 'Bilgisayar Mühendisliði', 1),
-(2, 'Ýnþaat Mühendisliði', 1),
-(3, 'Elektrik Elektronik Mühendisliði', 1),
-(4, 'Hemþirelik', 2),
+(1, 'Bilgisayar MÃ¼hendisliÃ°i', 1),
+(2, 'Insaat MÃ¼hendisligi', 1),
+(3, 'Elektrik Elektronik MÃ¼hendisligi', 1),
+(4, 'Hemsirelik', 2),
 (5, 'Fizyoterapi', 2),
 (6, 'Beslenme ve Diyetetik', 2),
 (7, 'Anayasa Hukuku', 3),
 (8, 'Ceza Hukuku', 3),
-(9, 'Uluslararasý Hukuk', 3);
+(9, 'Uluslararasi Hukuk', 3);
