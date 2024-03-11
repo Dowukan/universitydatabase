3c67d63ea05637c88c0625b435b065d94d01b61d
Doğukan Çalışkan <80880586+Dowukan@users.noreply.github.com>
2024-03-11 18:15:26 +0300

    Update Ogretmenler.sql

--- Ogretmenler.sql
+++ Ogretmenler.sql
@@ -1,29 +1,29 @@
 
 INSERT INTO Ogretmen (OgretmenID, Ad, Soyad, DogumTarihi, Cinsiyet, BolumID)
 VALUES
-(1, 'İlyas', 'Özer', '1970-03-15', 'E', 1),
+(1, 'Ilyas', 'Ã–zer', '1970-03-15', 'E', 1),
 (2, 'Bahar', 'Milani', '1982-05-20', 'K', 1),
-(3, 'Mehmet Akif', 'Çiftçi', '1975-11-10', 'E', 2),
-(4, 'Necla', 'Bandırmalı Ertürk', '1980-02-25', 'K', 2),
+(3, 'Mehmet Akif', 'Ã‡iftÃ§i', '1988-11-10', 'E', 2),
+(4, 'Necla', 'Bandirmali ErtÃ¼rk', '1980-02-25', 'K', 2),
 (5, 'Muhammed', 'Milani', '1978-09-03', 'E', 3),
-(6, 'Arzum', 'Karataş', '1983-06-18', 'K', 3);
+(6, 'Arzum', 'Karatas', '1983-06-18', 'K', 3);
 
 
 INSERT INTO Ogretmen (OgretmenID, Ad, Soyad, DogumTarihi, Cinsiyet, BolumID)
 VALUES
-(7, 'Can', 'Yıldız', '1972-04-18', 'E', 4),
-(8, 'Deniz', 'Koç', '1985-07-20', 'K', 4),
+(7, 'Can', 'YÃ½ldÃ½z', '1972-04-18', 'E', 4),
+(8, 'Deniz', 'KoÃ§', '1985-07-20', 'K', 4),
 (9, 'Emre', 'Demir', '1979-12-10', 'E', 5),
-(10, 'Gülay', 'Kurt', '1988-03-25', 'K', 5),
-(11, 'Hakan', 'Şahin', '1981-08-03', 'E', 6),
-(12, 'İpek', 'Aydın', '1986-05-18', 'K', 6);
+(10, 'GÃ¼lay', 'Kurt', '1988-03-25', 'K', 5),
+(11, 'Hakan', 'Ãahin', '1981-08-03', 'E', 6),
+(12, 'Ãpek', 'AydÃ½n', '1986-05-18', 'K', 6);
 
 
 INSERT INTO Ogretmen (OgretmenID, Ad, Soyad, DogumTarihi, Cinsiyet, BolumID)
 VALUES
-(13, 'Ayşenur', 'Elmas', '1973-06-17', 'K', 7),
+(13, 'AyÃ¾enur', 'Elmas', '1973-06-17', 'K', 7),
 (14, 'Kutlu Kaan', 'Demir', '1984-09-22', 'E', 7),
 (15, 'Leyla', 'Mecnun', '1977-10-13', 'K', 8),
 (16, 'Arslan', 'Kutay', '1982-01-28', 'E', 8),
-(17, 'Hatice Nur', 'Aydın', '1980-12-05', 'K', 9),
+(17, 'Hatice Nur', 'AydÃ½n', '1980-12-05', 'K', 9),
 (18, 'Ozan', 'Kabak', '1985-04-20', 'E', 9);
